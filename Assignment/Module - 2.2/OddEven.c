fcd52bb1f58207fd955db8e5ddb148b8ea972521
DishaKhunt <124360419+DishaKhunt@users.noreply.github.com>
2023-05-06 17:36:31 +0530

    Add files via upload

---
+++ Assignment/Module - 2.2/OddEven.c
@@ -0,0 +1,14 @@
+//WAP to find number is even or odd using ternary operator
+
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter the value of a : " );
+    scanf("%d", &a);
+
+    (a%2 == 0) ?  (printf("%d is a Even number.",a)) : (printf("%d is a Odd number.",a));
+
+    return 0;
+}
