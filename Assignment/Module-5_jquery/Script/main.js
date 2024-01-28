f68c3d3b8ca479691851389bb0a6f2f160296568
DishaKhunt <124360419+DishaKhunt@users.noreply.github.com>
2024-01-28 20:43:15 +0530

    Add files via upload

--- Assignment/Module-5_jquery/Script/main.js
+++ Assignment/Module-5_jquery/Script/main.js
@@ -1,7 +1,16 @@
+
 $(document).ready(function(){
+    var images = ['#img1', '#img2', '#img3'];
+    var index = 0;
+
     $('#btn').click(function(){
-        $('#img1').animate({ left: '100%' });
-        $('#img2').animate({ left: '100%' });
-        $('#img3').animate({ left: '100%' });
+        // Move the current image to the left
+        $(images[index]).animate({ left: '100%' });
+        
+        // Increment the index
+        index = (index + 1) % images.length;
+        
+        // Move the next image to its initial position
+        $(images[index]).css({ left: '100%' }).animate({ left: '0' });
     });
 });
